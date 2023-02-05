23afedaddb8f2cb90977a4c09d1faa73758b71cf
Michał Masiulanis <m.masiulanis@gmail.com>
2023-02-05 12:34:21 +0100

    for check

--- src/js/index.js
+++ src/js/index.js
@@ -6,24 +6,14 @@ const searchBtn = document.getElementById('search-btn');
 const pictureGallery = document.querySelector('.gallery');
 
 const KEY = '33302175-33178da1359f032779e0154a7';
-const query = searchInput.value;
 const imageType = 'photo';
 const orientation = 'horizontal';
 const safeSearch = true;
-
-const URL =
-  'https://pixabay.com/api/?key=' +
-  KEY +
-  '&q=' +
-  query +
-  '&image_type=' +
-  imageType +
-  '&orientation=' +
-  orientation +
-  'safesearch=' +
-  safeSearch;
+// const page = 1;
+const perPage = 10;
 
 function clearGallery() {
+  //   document.querySelector('.photo-car').remove();
   pictureGallery.innerHTML = '';
 }
 
@@ -31,22 +21,22 @@ function createGallery(imgSrc, imgAlt, likes, views, comments, downloads) {
   const pictureDiv = document.createElement('div');
   pictureDiv.innerHTML = `
   <div class="photo-card">
-  <img src="${imgSrc}" alt="${imgAlt}" loading="lazy" width="300px"/>
+  <img src="${imgSrc}" alt="${imgAlt}" loading="lazy"/>
   <div class="info">
     <p class="info-item">
-      <b>Likes</b>
+      <b>Likes:</b>
       <span>${likes}</span>
     </p>
     <p class="info-item">
-      <b>Views</b>
+      <b>Views:</b>
       <span>${views}</span>
     </p>
     <p class="info-item">
-      <b>Comments</b>
+      <b>Comments:</b>
       <span>${comments}</span>
     </p>
     <p class="info-item">
-      <b>Downloads</b>
+      <b>Downloads:</b>
       <span>${downloads}</span>
     </p>
   </div>
@@ -55,28 +45,49 @@ function createGallery(imgSrc, imgAlt, likes, views, comments, downloads) {
   pictureGallery.append(pictureDiv);
 }
 
-function findPictures(searchQuery) {
+function findPictures(query) {
+  const URL =
+    'https://pixabay.com/api/?key=' +
+    KEY +
+    '&q=' +
+    query +
+    '&image_type=' +
+    imageType +
+    '&orientation=' +
+    orientation +
+    'safesearch=' +
+    safeSearch +
+    // 'page=' +
+    // 1 +
+    'per_page=' +
+    perPage;
+  console.log(URL);
   fetch(URL)
     .then(r => {
       if (r.ok) {
         return r.json();
       } else {
-        Notiflix.Notify.warning('Opps, nothing found.');
+        Notiflix.Notify.warning(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
         clearGallery();
       }
     })
     .then(r => {
-      console.log(r);
-      //   const hits = r['hits'];
       //   console.log(hits);
+      const hits = r['hits'];
+      console.log(hits);
+      const totalHits = r['totalHits'];
+      Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+
       for (let i = 0; ; i++) {
         createGallery(
-          r['hits'][i]['largeImageURL'],
-          r['hits'][i]['tags'],
-          r['hits'][i]['likes'],
-          r['hits'][i]['views'],
-          r['hits'][i]['comments'],
-          r['hits'][i]['downloads']
+          hits[i]['previewURL'],
+          hits[i]['tags'],
+          hits[i]['likes'],
+          hits[i]['views'],
+          hits[i]['comments'],
+          hits[i]['downloads']
         );
       }
     })
@@ -87,7 +98,7 @@ searchBtn.addEventListener('click', e => {
   e.preventDefault();
   clearGallery();
   if (searchInput.value.trim() != '') {
-    findPictures(query);
+    findPictures(searchInput.value);
   } else {
     Notiflix.Notify.warning('Type any word in the search box!');
   }
